4b8c755b99f0718febb958e810dca7b7d51f26f4
Jamie Morrissey <jamiemorrissey@gmail.com>
2022-09-02 23:16:03 -0700

    fix bug about asking name

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-a chatbot is an artificial intelligence-powered piece of software in a device (Siri, Alexa, Google Assistant) application, website, or other networks.  History of chatbot dates back to 1966 when a computer program called ELIZA was invented by Weizenbaum. <br />
+a chatbot is an artificial intelligence-powered piece of software in a device (Apple Cortan, Siri, Alexa, Google Assistant) application, website, or other networks.  History of chatbot dates back to 1966 when a computer program called ELIZA was invented by Weizenbaum. <br />
 
 
 Machine Learning is a field of inquiry devoted tounderstanding and building methods that 'learn'
--- bot.py
+++ bot.py
@@ -1,12 +1,12 @@
 
 import nltk
-import numpy as np
+# import numpy as np
 import random
 import string
 
 # https://python.gotrained.com/chatbot-development-python-nltk/
-greeting_input_texts = ("hey", "heys", "hello", "morning", "evening", "greetings", "question")
-greeting_reply_texts = ["hey", "hey hows you?", "*nod", "hello there", "ello", "Welcome", "what is the answer?"]
+greeting_input_texts = ("What's your name?", "hey", "heys", "hello", "morning", "evening", "greetings", "question")
+greeting_reply_texts = ["They call me {0}.format(user_name)","hey", "hey hows you?", "*nod", "hello there", "ello", "Welcome", "what is the answer?"]
 
 def reply_greeting(text): # loop through list of words
 
@@ -15,6 +15,8 @@ def reply_greeting(text): # loop through list of words
             return random.choice(greeting_reply_texts)
 
 continue_discussion=True
+print("BOT: What do you want me to call you?")
+user_name = input()
 print("Hello, I am a chatbot, I will answer your queries regarding global warming:")
 while (continue_discussion==True):
     user_input = input()
